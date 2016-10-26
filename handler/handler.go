ba31f3f5dc4e07963a7728e91e65465f40df6e17
Rafał Gajdulewicz <rafax@users.noreply.github.com>
2016-10-26 12:55:52 +0100

    Fix gometalinter warnings (#2)
    * Fix gometalinter warnings
    
    * Fix fasthttprouter usage for new API

--- handler/handler.go
+++ handler/handler.go
@@ -2,6 +2,7 @@ package handler
 
 import "time"
 
+// SubscriptionData represents the data for a single user subscription
 type SubscriptionData struct {
 	UserId    string
 	ExpiresAt time.Time
@@ -9,14 +10,18 @@ type SubscriptionData struct {
 	Platform  string
 }
 
+// Equal returns true if a two instances of SubscriptionData are equal to each other
+// It is necessary due to the fact that ExpiresAt cannot be reliably compared with ==
 func (sd SubscriptionData) Equal(u SubscriptionData) bool {
 	return sd.UserId == u.UserId && sd.ExpiresAt.Equal(u.ExpiresAt) && sd.Level == u.Level && sd.Platform == u.Platform
 }
 
+// Token represents a token issued to the user
 type Token interface {
 	String() string
 }
 
+// StringToken represents the Token as a string
 type StringToken struct {
 	Token string
 }
@@ -25,6 +30,7 @@ func (s StringToken) String() string {
 	return s.Token
 }
 
+// TokenHandler converts between SuscriptionData and Token
 type TokenHandler interface {
 	Encrypt(SubscriptionData) (Token, error)
 	Decrypt(Token) (SubscriptionData, error)

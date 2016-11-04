a3387b7738739e4e74887aed134b57f97fd73370
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2016-11-04 17:25:09 +0100

    Protect map with RWLock

--- token/memory.go
+++ token/memory.go
@@ -2,22 +2,28 @@ package token
 
 import (
 	"errors"
+	"sync"
 
 	"github.com/satori/go.uuid"
 )
 
 type MemoryHandler struct {
 	data map[string]SubscriptionData
+	lock sync.RWMutex
 }
 
 func (h *MemoryHandler) Encrypt(sd SubscriptionData) (Token, error) {
 	token := uuid.NewV4()
+	h.lock.Lock()
 	h.data[token.String()] = sd
+	h.lock.Unlock()
 	return token, nil
 }
 
 func (h *MemoryHandler) Decrypt(t Token) (SubscriptionData, error) {
+	h.lock.RLock()
 	sd, ok := h.data[t.String()]
+	h.lock.RUnlock()
 	if !ok {
 		return SubscriptionData{}, errors.New("No subscription found for token")
 	}

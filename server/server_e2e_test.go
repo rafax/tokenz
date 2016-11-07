3543582a6b38d2e18c101c4c716e33d594d226f0
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2016-11-07 20:40:17 +0100

    Handle errors

--- server/server_e2e_test.go
+++ server/server_e2e_test.go
@@ -27,38 +27,49 @@ func handlerTest(method string, token *string) func(t *testing.T) {
 	return func(t *testing.T) {
 		t.Run("Fetch token", func(t *testing.T) {
 			resp, err := http.Post(fmt.Sprintf("http://:8888/%v/%v/1000/all/mobilez", method, userID), "application/json", nil)
-			data := getJSON(resp, err, t)
+			data, err := getJSON(resp, err, t)
+			if err != nil {
+				t.Errorf("Could not get the token from response: %v\n", err)
+				return
+			}
 			tkn, ok := data.Path("token").Data().(string)
 			if !ok {
 				t.Errorf("Token not found in response when getting token: %v", data.String())
+				return
 			}
 			token = &tkn
 		})
 		t.Run("Fetch data for token", func(t *testing.T) {
 			resp, err := http.Get(fmt.Sprintf("http://:8888/%v/%v", method, *token))
-			data := getJSON(resp, err, t)
+			data, err := getJSON(resp, err, t)
+			if err != nil {
+				t.Errorf("Could not get subscription data from response: %v", err)
+				return
+			}
 			uid, ok := data.Path("UserId").Data().(string)
 			if !ok {
 				t.Errorf("UserId not found in response: %v", data.String())
+				return
 			}
 			if uid != userID {
 				t.Errorf("Invalid userId %v expected %v", uid, userID)
+				return
 			}
 		})
 	}
 }
 
-func getJSON(resp *http.Response, err error, t *testing.T) *gabs.Container {
+func getJSON(resp *http.Response, err error, t *testing.T) (*gabs.Container, error) {
 	if err != nil {
-		t.Errorf("Error when getting token: %v", err)
+		return nil, err
 	}
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		t.Errorf("Error reading response body: %v", err)
+		return nil, err
 	}
 	data, err := gabs.ParseJSON(body)
 	if err != nil {
-		t.Errorf("Error parsing JSON: %v", err)
+		return nil, err
 	}
-	return data
+	return data, nil
 }
